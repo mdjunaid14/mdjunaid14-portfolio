3d99f53935271ed7d7c20c7916cbff6c98f5dcbf
mdjunaid14 <mdjunaid14@outlook.com>
2019-12-20 18:21:47 +0530

    Initial Commit

---
+++ precache-manifest.532c00856ecbdcc186cdc35f2264da4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e0d1807badbb205e0d64c34a04b2af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e8b9a302c68b924bcb1",
+    "url": "/static/css/main.6cb68675.chunk.css"
+  },
+  {
+    "revision": "7f68eed98d39e578e580",
+    "url": "/static/js/2.6363bd47.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6363bd47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e8b9a302c68b924bcb1",
+    "url": "/static/js/main.d2898d24.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "f521c79a1fadb7929d0ad15879e2f0cd",
+    "url": "/static/media/21Clones.f521c79a.svg"
+  },
+  {
+    "revision": "1d66eb63b110a0db1f89084a5ae013b8",
+    "url": "/static/media/Income-Tax_Calculator_screenshot.1d66eb63.png"
+  }
+]);
